f893967ff1c103d7f0e8fcab905ffc068a053f1e
unknown <lukass6lgs@gmail.com>
2023-04-19 04:40:47 -0700

    Veikta virtulu krisana

---
+++ Assets/Skripti/VirtuluGenerators.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtuluGenerators : MonoBehaviour{
+
+    public GameObject[] virtuluSagataves;
+    public float laikaIntervals = 1.0f;
+    float minPoz, maxPoz;
+    Transform krasnsTransform;
+
+
+    void Start()
+    {
+        krasnsTransform = GetComponent<Transform>();
+    }
+
+    public void sagatavotVirtlus(bool stavkolis)
+    {
+        if (stavkolis) StartCoroutine(Generet());
+
+        else StopAllCoroutines();
+    }
+
+    IEnumerator Generet()
+    {
+        while (true)
+        {
+           // minPoz = krasnsTransform.position.x - 20;
+            minPoz = krasnsTransform.position.x + 20;
+            var cipars = Random.Range(minPoz, maxPoz);
+            var pozicija = new Vector2(cipars, transform.position.y);
+            GameObject virtulis = Instantiate(virtuluSagataves[Random.Range(0, virtuluSagataves.Length)], pozicija, Quaternion.identity, krasnsTransform);
+            yield return new WaitForSeconds(laikaIntervals);
+        }
+    }
+
+}
